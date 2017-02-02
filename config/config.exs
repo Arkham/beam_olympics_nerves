632f065e61b5efd0d06a11aa0c2ae0a4ad721747
Ju Liu <ju.liu@welaika.com>
2017-02-02 10:13:09 +0000

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,32 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Import target specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+# Uncomment to use target specific configurations
+
+# import_config "#{Mix.Project.config[:target]}.exs"
+
+config :mnesia,
+  debug: true
+
+config :sumo_db,
+  log_queries: true,
+  query_timeout: 30_000,
+  wpool_opts: [overrun_warning: 100],
+  storage_backends: [],
+  stores: [{
+    :bo_store_mnesia,
+    :sumo_store_mnesia,
+    [workers: 10, disc_copies: :here]
+  }],
+  docs: [{
+    :players,
+    :bo_store_mnesia,
+    %{module: :bo_players}
+  }],
+  events: []
